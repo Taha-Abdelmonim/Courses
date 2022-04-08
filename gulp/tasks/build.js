32efd9ef9650aa0ba7398c3fbb88e6279cc68106
Taha-Abdelmonim <taha.abdelmoonim@gmail.com>
2022-04-08 15:48:18 +0200

    md2

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -8,8 +8,8 @@ const { parallel, series, task } = require("gulp"),
   { cleanup } = require("./cleanup"),
   browserSync = require("browser-sync").create();
 
-task("serving", parallel(watcher, server));
-const build = series(cleanup, parallel(css, js, jsGlobal, webFonts, staticFiles), html, "serving");
+// task("serving", parallel(watcher, server));
+const build = series(cleanup, parallel(css, js, jsGlobal, webFonts, staticFiles), html);
 
 module.exports = {
   build,
